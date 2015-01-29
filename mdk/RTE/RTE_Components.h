d40278aee86695c5807ea93f2b8c665c6da70154
Manuel Limones <limonesu.me@gmail.com>
2015-01-29 00:29:42 -0600

    Create Keil proyect with main.c file

---
+++ mdk/RTE/RTE_Components.h
@@ -0,0 +1,14 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'filtro_fir_lp' 
+ * Target:  'Target 1' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+#endif /* RTE_COMPONENTS_H */
---
+++ source/main.c
@@ -0,0 +1,4 @@
+#include "TM4C123.h"                    // Device header
+int main(){
+	return 0;
+}
