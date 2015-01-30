9b6a86485bcaf4cf275a8eca39c2cf03e0cbd467
Manuel Limones <limonesu.me@gmail.com>
2015-01-29 20:15:57 -0600

    Modify adc.c to ver2 (not functional code)

--- source/ADC.c
+++ source/ADC.c
@@ -1,65 +1,74 @@
 #include "TM4C123.h"                    // Device header
 
-
 void config_ADC(void);
 void start_ADC(void);
 
 int ADC_samples[4];
-int ADC_samplesOk;
-int ADC_samplesEND=1;
+int ADC_sampleOk=0;
+int ADC_sampleEnd=1;
 int a=0;
+
+
 int main(){
-	config_ADC();
+	ADC_samples[0]=0;
+	ADC_samples[1]=0;
+	ADC_samples[2]=0;
+	ADC_samples[3]=0;
 	
+	config_ADC();
+	NVIC_EnableIRQ(ADC0SS0_IRQn); 
 	while(1){
-		if (ADC_samplesEND){
-		start_ADC();
-		}
+		if(ADC_sampleEnd){
+			start_ADC();
+	}
 		a++;
-		
-		
 	}
 }
 
-void config_ADC(void){
-	//configuracion de pines 
-	SYSCTL->RCGCGPIO|=(0x1<<4)|(0x1<<1)|(0x1<<3)|(0x1<<0);//habilitamos solo los gPIO que contenga las ADC analogico 
+void start_ADC(){
+	ADC0->PSSI=(0x1<<0);//inicia secuendiador 0
+	ADC_sampleEnd=0;
+}
+
+void config_ADC(){
+	
+	/*Configuracion de Pines*/
+	
+	SYSCTL->RCGCGPIO=(0x1<<4)|(0x1<<1)|(0x1<<3);;
+	SYSCTL->RCGCADC|=0x01;//habilitamos ADC0
+	
 	GPIOE->DEN&=~((0x1<<4)|(0x1<<5));
 	GPIOB->DEN&=~((0x1<<4)|(0x1<<5));
-	GPIOE->AMSEL|=((0x1<<4)|(0x1<<5)); 
-	GPIOB->AMSEL|=((0x1<<4)|(0x1<<5)); 
-	
-	//Configuracion ADC principal 
-	SYSCTL->RCGCADC|=(0x1);//habilitamos el adc 0 
-	ADC0->ACTSS&=~(0xF);//desactivacion de los secuenciadores durante la configuracion 
-	ADC0->EMUX  = (0x5<<0);//  para habilitar el trigger por timer  para scc0 
+	GPIOE->AMSEL|=(0x1<<4)|(0x1<<5);
+	GPIOB->AMSEL|=(0x1<<4)|(0x1<<5);
 	
 	
-	//configuracion del secuenciador 
-	ADC0->SSMUX0=(0x8)|(0x9<<4)|(0xA<<8)|(0xB<<12); //ordenamiento de forma de sampleo (recuerda la asignacion es en letra o enible alto)
-	ADC0->SSCTL0=(0x1<<14)|(0x1<<13);//final de secuencia en muestra 3 y genera la interrupcion 
-	ADC0->IM|=(0x1<<0); //interrupcion por mascara 
-	ADC0->ACTSS|=(0x1<<0);//Activando los secuenciadores del 0  
+	//Configuracion ADC principal
+	
+	ADC0->ACTSS&=~(0xF);//desactivamos secuenciadores durantes la configuracion
+	ADC0->EMUX=0; //para trigger por sofware (default)
+	
+	//Configuracion del sequenciador
+	ADC0->SSMUX0=(0x8)|(0x9<<4)|(0xA<<8)|(0xB<<12);
+	ADC0->SSCTL0=(0x1<<14)|(0x1<<13);//Final de secuencia en muestra 3, y genera interrupcion
+	//Desenmacaramos interrupcion por secuenciador 0
+	ADC0->IM|=(0x1<<0);
+	ADC0->ACTSS|=(0x1<<0);//activamos secuenciador 0
 }
-void ADC0_Handler(void){
-	ADC0->ISC=(0x1<<0);//limpieando la interrupcion por sample 0  
-	//leer fifo0
+
+void ADC0SS0_Handler(){
 	int i;
+	ADC0->ISC=(0x1<<0);//Limpiamos interrupcion por secuenciador 0
 	for(i=0;i<4;i++){
-		if(ADC0->SSFSTAT0&(0x1<<8)){
-		break;
+		if((ADC0->SSFSTAT0&(0x1<<8))){
+			 break;
 		}
-	   ADC_samples[i]=ADC0->SSFIFO0;	
+		ADC_samples[i]=ADC0->SSFIFO0;
 	}
 	if(((ADC0->SSFSTAT0&(0x1<<8))!=0)&&i==4){
-	ADC_samplesOk=1;
+			ADC_sampleOk=1;
+	}else{
+		  ADC_sampleOk=0;
 	}
-	else{
-	ADC_samplesOk=0;
-	}
-}
-void start_ADC(){
-  ADC0->PSSI=(0x1<<0);//inicia el secuenciador 0
-  
-	
+	ADC_sampleEnd=1;
 }
