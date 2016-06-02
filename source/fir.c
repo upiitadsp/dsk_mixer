fd6292f579d9ee2ce5f370caa1da27083ddd0285
Manuel Limones <limonesu.me@gmail.com>
2016-06-02 00:21:02 -0500

    Fix compilation error c89

--- source/fir.c
+++ source/fir.c
@@ -30,31 +30,32 @@ float gain_4 = 1;
 float x_fir[FIR_ORDER];
 
 void fir_init(const float *h_coff_0,const float *h_coff_1,const float *h_coff_2,const float *h_coff_3,const float *h_coff_4){
-    for(int i=0; i<FIR_ORDER; i++){
+    int i = 0;
+    for(i=0; i<FIR_ORDER; i++){
         h_fir_0[i]=h_coff_0[(FIR_ORDER-1)-i];
         h_fir_0[i+FIR_ORDER]=h_coff_0[(FIR_ORDER-1)-i];
     }
     center_pointer_0 = &h_fir_0[(FIR_ORDER-1)];
 
-    for(int i=0; i<FIR_ORDER; i++){
+    for(i=0; i<FIR_ORDER; i++){
         h_fir_1[i]=h_coff_1[(FIR_ORDER-1)-i];
         h_fir_1[i+FIR_ORDER]=h_coff_1[(FIR_ORDER-1)-i];
     }
     center_pointer_1 = &h_fir_1[(FIR_ORDER-1)];
 
-    for(int i=0; i<FIR_ORDER; i++){
+    for(i=0; i<FIR_ORDER; i++){
         h_fir_2[i]=h_coff_2[(FIR_ORDER-1)-i];
         h_fir_2[i+FIR_ORDER]=h_coff_2[(FIR_ORDER-1)-i];
     }
     center_pointer_2 = &h_fir_2[(FIR_ORDER-1)];
 
-    for(int i=0; i<FIR_ORDER; i++){
+    for(i=0; i<FIR_ORDER; i++){
         h_fir_3[i]=h_coff_3[(FIR_ORDER-1)-i];
         h_fir_3[i+FIR_ORDER]=h_coff_3[(FIR_ORDER-1)-i];
     }
     center_pointer_3 = &h_fir_3[(FIR_ORDER-1)];
 
-    for(int i=0; i<FIR_ORDER; i++){
+    for(i=0; i<FIR_ORDER; i++){
         h_fir_4[i]=h_coff_4[(FIR_ORDER-1)-i];
         h_fir_4[i+FIR_ORDER]=h_coff_4[(FIR_ORDER-1)-i];
     }
@@ -70,6 +71,7 @@ float fir_filter(float new_sample){
     float accumulator_3 = 0;
     float accumulator_4 = 0;
     float y_out = 0;
+    int i;
 
     x_fir[head_pointer] = new_sample;
 
@@ -79,7 +81,7 @@ float fir_filter(float new_sample){
     h_response_3 = center_pointer_3 - head_pointer;
     h_response_4 = center_pointer_4 - head_pointer;
 
-    for(int i=0;i<FIR_ORDER;i++){
+    for(i=0;i<FIR_ORDER;i++){
         accumulator_0 += x_fir[i] * h_response_0[i];
         accumulator_1 += x_fir[i] * h_response_1[i];
         accumulator_2 += x_fir[i] * h_response_2[i];
